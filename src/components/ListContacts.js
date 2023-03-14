cb234aa5b429ada352fb76717a71b4ac6cf7ccb9
PhilipDeFraties <philipjfry338@gmail.com>
2023-03-13 19:23:56 -0600

    Adds removeContact function to App.js, passes to ListContacts component to use as onClick callback for delete button

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,20 +1,26 @@
-const ListContacts = ({ contacts }) => {
+const ListContacts = ({ contacts, onDeleteContact }) => {
     return (
         <ol className="contact-list">
             {
                 contacts.map((contact) => (
                     <li key={contact.id} className="contact-list-item">
                         <div 
-                        className="contact-avatar" 
-                        style={{
-                            backgroundImage: `url(${contact.avatarURL})`,
-                        }}>
+                            className="contact-avatar" 
+                            style={{
+                                backgroundImage: `url(${contact.avatarURL})`,
+                            }}
+                        >
                         </div>
                         <div className="contact-details">
                             <p>{contact.name}</p>
                             <p>{contact.handle}</p>
                         </div>
-                        <button className="contact-remove">Remove</button>
+                        <button 
+                            className="contact-remove" 
+                            onClick={() => onDeleteContact(contact)}
+                        >
+                            Remove
+                        </button>
                     </li>
                 ))
 
