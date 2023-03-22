f0302179480c9c247c1b260cfa6daaa0dffbfc06
PhilipDeFraties <philipjfry338@gmail.com>
2023-03-21 18:08:16 -0600

    Adds logic to CreateContact component, takes callback prop, renders form with fields for new contact

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,12 +1,36 @@
-import { useLocation } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import ImageInput from "./ImageInput";
+import serializeForm from "form-serialize";
 
-const CreateContact = () => {
-    let { state } = useLocation();
+const CreateContact = ({ onCreateContact }) => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const values = serializeForm(e.target, { hash: true });
 
-    console.log(state)
+        if (onCreateContact) {
+            onCreateContact(values);
+        }
+    };
+
     return (
-        <div>Create Contact</div>
-    )
+        <div>
+            <Link className="close-create-contact" to="/">
+                Close
+            </Link>
+            <form onSubmit={handleSubmit} className="create-contact-form">
+                <ImageInput
+                    className="create-contact-avatar-input"
+                    name="avatarURL"
+                    maxHeight={64}
+                />
+                <div className="create-contact-details">
+                    <input type="text" name="name" placeholder="Name" />
+                    <input type="text" name="handle" placeholder="Handle" />
+                    <button>Add Contact</button>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default CreateContact;
